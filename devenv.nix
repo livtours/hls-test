f3a0b680d1e6c5ce34c1487883c51bf356a35343
Dario Oddenino <branch13@gmail.com>
2025-11-07 15:28:15 +0100

    magic

--- devenv.nix
+++ devenv.nix
@@ -9,16 +9,6 @@ let
     import inputs.nixpkgs-unstable { system = pkgs.stdenv.system; };
 
 in {
-  dotenv.enable = true; # .env
-  dotenv.disableHint = true; # .env
-
-  # VSCode devcontainer https://devenv.sh/integrations/codespaces-devcontainer/
-  devcontainer.enable = true;
-
-  # https://devenv.sh/packages/
-  packages = [
-    pkgs.ghcid
-  ];
 
   # https://devenv.sh/languages/
   languages.nix.enable = true;
@@ -27,22 +17,22 @@ in {
   languages.haskell.languageServer = hsPkgs.haskell-language-server;
 
   # TODO: devenv 1.8.2+ does most of this by default
-  languages.haskell.stack = let
-  in pkgs.symlinkJoin {
-    name = "stack"; # will be available as the usual `stack` in terminal
-    paths = [ pkgsUnstable.stack ];
-    buildInputs = [ pkgs.makeWrapper ];
-    # NOTE: we use hpack on purpuse from pkgs/devenv-rolling channel
-    # This makes sure that stack uses the same hpack as we do when
-    # we run hpack manually. Ideally we would have languages.haskell.hpack option
-    # to overwrite the default hpack package but its not available yet.
-    postBuild = ''
-      wrapProgram $out/bin/stack \
-        --add-flags "--no-nix --system-ghc --no-install-ghc --with-hpack ${
-          lib.getExe pkgs.hpack
-        }"
-    '';
-  };
+  # languages.haskell.stack = let
+  # in pkgs.symlinkJoin {
+  #   name = "stack"; # will be available as the usual `stack` in terminal
+  #   paths = [ pkgsUnstable.stack ];
+  #   buildInputs = [ pkgs.makeWrapper ];
+  #   # NOTE: we use hpack on purpuse from pkgs/devenv-rolling channel
+  #   # This makes sure that stack uses the same hpack as we do when
+  #   # we run hpack manually. Ideally we would have languages.haskell.hpack option
+  #   # to overwrite the default hpack package but its not available yet.
+  #   postBuild = ''
+  #     wrapProgram $out/bin/stack \
+  #       --add-flags "--no-nix --system-ghc --no-install-ghc --with-hpack ${
+  #         lib.getExe pkgs.hpack
+  #       }"
+  #   '';
+  # };
 
 
   # NOTE: profiles are available on devenv 1.9
